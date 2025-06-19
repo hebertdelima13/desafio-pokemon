3e4fe3de47e51cafd51a98a99f08271481894371
hebertdelima13 <hebertdelima13@gmail.com>
2025-06-19 16:26:21 -0300

    ✨ feat: implement home page with pokemon search and pagination

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { IonSearchbar, IonHeader, IonContent, IonCol, IonGrid, IonRow, IonButton } from '@ionic/angular/standalone';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [],
+  imports: [IonSearchbar, IonHeader, IonContent, IonCol, IonGrid, IonRow, IonButton],
 })
 export class HomePage {
-  constructor() {}
+  constructor(public pokemonService: PokemonService, private router: Router) {}
+
+  goToDetails(pokemon: any) {
+    this.router.navigate(['/details', pokemon.name]);
+  }
+
+  nextPage() {
+    this.pokemonService.nextPage();
+  }
+
+  previousPage() {
+    this.pokemonService.previousPage();
+  }
 }
--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,9 +1,55 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { computed, Injectable, signal } from '@angular/core';
+import { map, Observable, tap } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PokemonService {
+  private readonly api = 'https://pokeapi.co/api/v2/pokemon';
 
-  constructor() { }
+  page = signal(0);
+  limit = 20;
+
+  pokemons = signal<any[]>([]);
+  favorites = signal<Set<number>>(new Set());
+
+  constructor(private http: HttpClient) {
+    this.loadPokemons();
+  }
+
+  loadPokemons() {
+    const offset = this.page() * this.limit;
+    this.http
+      .get<any>(`${this.api}?offset=${offset}&limit=${this.limit}`)
+      .pipe(
+        map((res) => res.results),
+        tap((list) => this.pokemons.set(list))
+      )
+      .subscribe();
+  }
+
+  nextPage() {
+    this.page.update((p) => p + 1);
+    this.loadPokemons();
+  }
+
+  previousPage() {
+    if (this.page() > 0) {
+      this.page.update((p) => p - 1);
+      this.loadPokemons();
+    }
+  }
+
+  getPokemonDetails(name: string): Observable<any> {
+    return this.http.get(`${this.api}/${name}`);
+  }
+
+  toggleFavorite(id: number) {
+    const favs = new Set(this.favorites());
+    favs.has(id) ? favs.delete(id) : favs.add(id);
+    this.favorites.set(favs);
+  }
+
+  isFavorite = (id: number) => computed(() => this.favorites().has(id));
 }
